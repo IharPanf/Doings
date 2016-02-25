b3d36a074feaeccca524c56d72b982c53664e07b
IharPanf <panfilenko.igor@gmail.com>
2016-02-25 16:28:14 +0300

    use JSX

---
+++ frontend/js/app.js
@@ -0,0 +1,40 @@
+var objEvents = {
+    "monday": [
+        {"idEvent": "1", "textEvent": "SomeEvents1"},
+        {"idEvent": "2", "textEvent": "SomeEvents2"},
+        {"idEvent": "3", "textEvent": "SomeEvents3"}
+    ],
+    "tuesday": [
+        {"idEvent": "1", "textEvent": "SomeEvents1"},
+        {"idEvent": "2", "textEvent": "SomeEvents2"},
+        {"idEvent": "3", "textEvent": "SomeEvents3"}
+    ],
+    "wednesday": [
+        {"idEvent": "1", "textEvent": "SomeEvents1"},
+        {"idEvent": "2", "textEvent": "SomeEvents2"},
+        {"idEvent": "3", "textEvent": "SomeEvents3"}
+    ],
+    "thusday": [
+        {"idEvent": "1", "textEvent": "SomeEvents1"},
+        {"idEvent": "2", "textEvent": "SomeEvents2"},
+        {"idEvent": "3", "textEvent": "SomeEvents3"}
+    ],
+    "Friday": [
+        {"idEvent": "1", "textEvent": "SomeEvents1"},
+        {"idEvent": "2", "textEvent": "SomeEvents2"},
+        {"idEvent": "3", "textEvent": "SomeEvents3"}
+    ],
+    "Saturday": [
+        {"idEvent": "1", "textEvent": "SomeEvents1"},
+        {"idEvent": "2", "textEvent": "SomeEvents2"},
+        {"idEvent": "3", "textEvent": "SomeEvents3"}
+    ],
+    "Sunday": [
+        {"idEvent": "1", "textEvent": "SomeEvents1"},
+        {"idEvent": "2", "textEvent": "SomeEvents2"},
+        {"idEvent": "3", "textEvent": "SomeEvents3"}
+    ]
+};
+
+
+
